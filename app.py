8bb9a45b8889888761881db49b633fdbb74c54e9
Sunil <skb@Sunils-MacBook-Pro.local>
2023-11-25 22:44:05 +0530

    new updated commit

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 def getdata():
     ravinshu=[2,7,11,15]
     return ravinshu
-def calculation(args):
-    pass
+def calculation(data):
+    sum=0
+    for i in data:
+        sum=sum+i
+        
+    return sum
+
 def finalresult(final):
     return final/2
 finalresult(calculation(getdata()))
